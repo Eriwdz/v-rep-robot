711e1deb1c9abc8dbf9fd9caf7c0d939f498bbeb
Obada Jabassini <obada.jabassini@gmail.com>
2018-05-18 07:32:39 +0300

    Initial Commit

---
+++ data_reader.py
@@ -0,0 +1,45 @@
+import os
+import pickle
+import random
+
+import cv2
+import dlib
+import numpy as np
+from imutils import resize
+
+
+class DataReader:
+    detector = dlib.get_frontal_face_detector()
+
+    @staticmethod
+    def read(root_path: str):
+        X, y = DataReader.read_gender(f"{root_path}/male", "male")
+        X1, y1 = DataReader.read_gender(f"{root_path}/female", "female")
+        X.extend(X1), y.extend(y1)
+        col = X[0].shape[0]
+        combined = list(zip(X, y))
+        random.shuffle(combined)
+        X[:], y[:] = zip(*combined)
+        return np.asarray(X).reshape(-1, col), np.asarray(y).reshape(-1)
+
+    @staticmethod
+    def read_gender(path, gender):
+        X, y = [], []
+        gender = int(gender == "male")
+        for file in os.listdir(path):
+            with open(f"{path}/{file}", "rb") as f:
+                temp = pickle.load(f, encoding="latin1")
+                X.append(temp)
+                y.append(gender)
+        return X, y
+
+    @staticmethod
+    def read_image(image_path: str):
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        rects = DataReader.detector(image, 2)
+        rect = rects[0]
+        image = image[rect.top():rect.bottom(), rect.left():rect.right()]
+        image = resize(image, 30, 30)
+        cv2.imwrite("test.jpg", image)
+        image = image.reshape(1, -1)
+        return image
---
+++ trainer.py
@@ -0,0 +1,38 @@
+from joblib import dump
+from sklearn.decomposition import PCA
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV
+# Load data
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+
+from data_reader import DataReader
+
+X, y = DataReader.read("data/preprocessed")
+# split into a training and testing set
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# Compute a PCA
+n_components = 100
+pca = PCA(n_components=n_components, whiten=True)
+pca.fit(X_train, y_train)
+
+params = {
+    'clf__C': [1, 10, 100, 1000],
+    'clf__gamma': [0.001, 0.0001],
+    'clf__kernel': ['rbf', "poly"],
+}
+
+pipeline = Pipeline([
+    ("pca", pca),
+    ("clf", SVC())
+])
+
+grid = GridSearchCV(pipeline, params)
+grid.fit(X_train, y_train)
+clf = grid.best_estimator_
+target_names = ["female", "male"]
+y_pred = clf.predict(X_test, y_test)
+print(classification_report(y_test, y_pred, target_names=target_names))
+dump(pca, "models/transformer.model")
+dump(clf, "models/classifier.model")
